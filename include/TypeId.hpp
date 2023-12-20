f2e161cdc850cc62d07a3980182496c38893720b
GustavoBodi <gustavorbodi@gmail.com>
2023-12-20 18:03:24 -0300

    Added docs for TypeId

--- include/TypeId.hpp
+++ include/TypeId.hpp
@@ -2,6 +2,10 @@
 
 extern std::atomic_uint64_t TypeIdCounter;
 
+/*!
+ * @brief Auxiliary function for returning an increasing id each template instantiation,
+ * return the same value for the same type
+ */
 template <typename T>
 int getTypeId() {
   static uint64_t id = ++TypeIdCounter;
