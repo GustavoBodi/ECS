689392c51b3fa55881914bd7d6a07fd22e7a1d15
GustavoBodi <gustavorbodi@gmail.com>
2023-12-29 15:43:47 -0300

    Finished documenting the type mapping

--- include/TypeMapper.hpp
+++ include/TypeMapper.hpp
@@ -1,6 +1,5 @@
 #include <unordered_map>
 #include <atomic>
-#include <iostream>
 
 template <typename Type>
 class TypeMapper {
@@ -8,74 +7,48 @@ class TypeMapper {
     typedef std::unordered_map<uint64_t, Type> MapperType;
 
   public:
-    /*!
-     * @brief Type alias for the iterator
-     */
+    /*! @brief Type alias for the iterator */
     using iterator = typename MapperType::iterator;
 
-    /*!
-     * @brief Type alias for the const iterator
-     */
+    /*! @brief Type alias for the const iterator */
     using const_iterator = typename MapperType::const_iterator;
 
-    /*!
-     * @brief Type alias for the value
-     */
+    /*! @brief Type alias for the value */
     using value_type = typename MapperType::value_type;
 
-    /*!
-     * @brief Const begin iterator
-     */
+    /*! @brief Const begin iterator */
     const_iterator begin() const { return mapper.begin(); }
 
-    /*!
-     * @brief Const end iterator
-     */
+    /*! @brief Const end iterator */
     const_iterator end() const { return mapper.end(); }
 
-    /*!
-     * @brief Begin iterator
-     */
+    /*! @brief Begin iterator */
     iterator begin() { return mapper.begin(); }
 
-    /*!
-     * @brief End iterator
-     */
+    /*! @brief End iterator */
     iterator end() { return mapper.end(); }
 
-    /*!
-     * @brief Returns the size
-     */
+    /*! @brief Returns the size */
     std::size_t size() { return _size; }
 
-    /*!
-     * @brief Returns the size as const
-     */
+    /*! @brief Returns the size as const */
     std::size_t size() const { return _size; }
 
-    /*!
-     * @brief Return true if the map contains the type
-     */
+    /*! @brief Return true if the map contains the type */
     template <typename ...Key>
     bool contains() const { 
       return mapper.contains(get_type_id<Key...>());
     }
 
-    /*!
-     * @brief Finds a mapping based on the type
-     */
+    /*! @brief Finds a mapping based on the type */
     template <typename ...Key>
     iterator find() { return mapper.find(get_type_id<Key...>()); }
 
-    /*!
-     * @brief Finds a mapping (const) based on the type
-     */
+    /*! @brief Finds a mapping (const) based on the type */
     template <typename ...Key>
     const_iterator find() const { return mapper.find(get_type_id<Key...>()); }
 
-    /*!
-     * @brief Removes based on a type mapping
-     */
+    /*! @brief Removes based on a type mapping */
     template <typename ...Key>
     Type remove() {
       auto value = find<Key...>()->second;
@@ -83,20 +56,7 @@ class TypeMapper {
       return value;
     }
 
-    /*!
-     * @brief Inserts in the type mapping, move semantics
-     */
-    //template <typename ...Key>
-    //uint64_t put(Type &&value) {
-    //  uint64_t id { get_type_id<Key...>() };
-    //  mapper[id] = std::forward<Type>(value);
-    //  ++_size;
-    //  return id;
-    //}
-
-    /*!
-     * @brief Inserts in the type mapping, copy (for pointers)
-     */
+    /*! @brief Inserts in the type mapping, copy (for pointers) */
     template <typename ...Key>
     uint64_t put(Type value) {
       uint64_t id { get_type_id<Key...>() };
@@ -105,25 +65,27 @@ class TypeMapper {
       return id;
     }
 
-    /*!
-     * @brief Returns the id (internal) from the type
-     */
+    /*! @brief Returns the id (internal) from the type */
     template <typename ...Key>
     uint64_t id() {
       return get_type_id<Key...>();
     }
 
   private:
-    std::size_t _size { 0 };
-    MapperType mapper;
+    /*! @brief Template trick for obtaining the id (for each type the id is always the same) */
     template <typename ...Key>
     inline static uint64_t get_type_id() {
       static const uint64_t id = LastTypeId++;
       return id;
     }
-
+    /*! @brief Amount of registered types */
+    std::size_t _size { 0 };
+    /*! @brief Unordered_map that maps the typeid to the expected value */
+    MapperType mapper;
+    /*! @brief Variable for the id */
     static std::atomic_uint64_t LastTypeId;
 };
 
+/*! @brief Template for the id */
 template <class ValueType>
 std::atomic_uint64_t TypeMapper<ValueType>::LastTypeId(0);
